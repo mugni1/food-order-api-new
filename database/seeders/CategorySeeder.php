ee416b1ecc77772c194e464f1d424e0db0f163c5
mugni1 <asepabdulmugni22@gmail.com>
2025-01-29 09:38:40 +0700

    add categorys migration, seeder , model

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Category::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $catgorys = [
+            ['name' => 'foods'],
+            ['name' => 'drinks'],
+        ];
+
+        collect($catgorys)->map(function ($category) {
+            Category::create([
+                'name' => $category['name'],
+            ]);
+        });
+    }
+}
